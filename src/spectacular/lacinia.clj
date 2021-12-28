96fe4025c61f6c6f41a6d7537041364167fc4d7d
Simon Cusack <simon.cusack@gmail.com>
2021-12-28 15:28:20 +1100

    Refactors enums to use new gql-type functions.

--- src/spectacular/lacinia.clj
+++ src/spectacular/lacinia.clj
@@ -116,40 +116,6 @@
 
 ;;;
 
-(defn ->gql-key
-  [ky & {:keys [::key]}]
-  (let [ky (or key
-               (sp/-get ky ::key)
-               ky)]
-    (if (s/ends-with? ky "?")
-      (-> ky
-          name
-          (s/replace #"\?$" "")
-          (s/replace #"^"   "is-")
-          csk/->camelCaseKeyword)
-      (csk/->camelCaseKeyword ky))))
-
-(defn ->gql-type-key
-  [ky & {:keys [::type]}]
-  ;; Sometimes we want to use the 'type' but in the 'key' position, so
-  ;; we translate for type but use the key case conversion.
-  (-> (or type
-          (sp/-get ky ::type)
-          ky)
-      csk/->PascalCaseKeyword))
-
-(defn ->gql-type
-  [ky & {:keys [token? input? required?] :as options}]
-  (let [gql-type (-> (cond-> (name (or (::type options)
-                                       (sp/-get ky ::type)
-                                       ky))
-                       token? (str "-token")
-                       input? (str "-input"))
-                     csk/->PascalCaseSymbol)]
-    (if required?
-      (list 'non-null gql-type)
-      gql-type)))
-
 ;;; -- Enums
 
 (defmulti enum->schema (fn [enum]
@@ -163,18 +129,21 @@
                   {:enum enum})))
 
 (defmethod enum->schema :enum-key
-  [ky]
-  (if-let [values (sp/values ky)]
+  [k]
+  (if-let [values (sp/values k)]
     ;; Enums are scalars so they have a type which maybe overridden on
     ;; the GQL side.  In the schema the type is in what we would
     ;; normally consider the 'key' position, so check for the GQL type
     ;; translation rather than a key translation.
-    [(->gql-type-key ky)
+    [(-> k
+         (gql-type-key  nil)
+         (gql-type-name nil))
      (-> {:values (mapv csk/->SCREAMING_SNAKE_CASE_KEYWORD values)}
-         (-assoc-description ky))]
+
+         (attr-description k))]
     ;; Do I need this warning?  Doesn't spec ensure I can't do this?
     (throw (ex-info "Can't transform to an enum, key doesn't have any values."
-                    {:key ky}))))
+                    {:enum-key k}))))
 
 (defmethod enum->schema :inline
   [{:keys [key values description]}]
